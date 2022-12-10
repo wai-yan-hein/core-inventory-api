b3b7b07b4f49ebf79b6e00e651e71ffd48b65df9
WaiYanHain <waiyanhain215@gmail.com>
2022-12-10 17:53:29 +0630

    w

--- src/main/java/cv/api/inv/dao/TraderDaoImpl.java
+++ src/main/java/cv/api/inv/dao/TraderDaoImpl.java
@@ -127,7 +127,7 @@ public class TraderDaoImpl extends AbstractDao<TraderKey, Trader> implements Tra
         } catch (Exception e) {
             log.error(e.getMessage());
         }
-        return Util1.getSyncDate();
+        return Util1.getOldDate();
     }
 
     @Override
