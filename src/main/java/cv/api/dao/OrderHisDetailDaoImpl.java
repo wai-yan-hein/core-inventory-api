8138f57eda10bfd1eb4f5ecb30b1fd1e41693c45
Ayethu-Sint <ayethusint@gmail.com>
2023-06-20 13:41:43 +0630

    01

--- src/main/java/cv/api/dao/OrderHisDetailDaoImpl.java
+++ src/main/java/cv/api/dao/OrderHisDetailDaoImpl.java
@@ -54,7 +54,7 @@ public class OrderHisDetailDaoImpl extends AbstractDao<OrderDetailKey, OrderHisD
 //                "op.batch_no = g.batch_no and\n" +
                 "op.comp_code = g.comp_code\n" +
                 "and op.dept_id = g.dept_id\n" +
-                "and g.deleted = 0\n" +
+                "and g.deleted = false\n" +
                 "left join trader t on g.trader_code = t.code\n" +
                 "and g.comp_code = t.comp_code\n" +
                 "and g.dept_id = t.dept_id\n" +
