f498b605bb3c63ee43ad95afeba91d9b2bb5192f
WaiYanHain <waiyanhain215@gmail.com>
2023-07-26 14:01:14 +0630

    w

--- src/main/java/cv/api/repo/AccountRepo.java
+++ src/main/java/cv/api/repo/AccountRepo.java
@@ -36,6 +36,8 @@ public class AccountRepo {
     @Autowired
     private TraderGroupService groupService;
     @Autowired
+    private GRNService grnService;
+    @Autowired
     private PurExpenseService purExpenseService;
     @Autowired
     private ExpenseService expenseService;
@@ -243,7 +245,6 @@ public class AccountRepo {
                 boolean deleted = sh.isDeleted();
                 String vouNo = sh.getKey().getVouNo();
                 String projectNo = sh.getProjectNo();
-                String batchNo = sh.getBatchNo();
                 double vouTotal = Util1.getDouble(sh.getVouTotal());
                 double vouDis = Util1.getDouble(sh.getDiscount());
                 double vouPaid = Util1.getDouble(sh.getPaid());
@@ -269,6 +270,17 @@ public class AccountRepo {
                     }
                     if (!Util1.isNullOrEmpty(t.getAccount())) {
                         balAcc = t.getAccount();
+                    }
+                }
+                String batchNo = "";
+                String grnVouNo = sh.getGrnVouNo();
+                if (grnVouNo != null) {
+                    GRNKey grnKey = new GRNKey();
+                    grnKey.setVouNo(grnVouNo);
+                    grnKey.setCompCode(compCode);
+                    GRN grn = grnService.findByCode(grnKey);
+                    if (grn != null) {
+                        batchNo = grn.getBatchNo();
                     }
                 }
                 List<Gl> listGl = new ArrayList<>();
